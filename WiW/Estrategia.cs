6e7c7dbff99e0f04a21d1183432aaddf66bb582a
chhristianc <cabrera.ch27@gmail.com>
2024-12-27 19:51:07 -0300

    Initial commit

---
+++ WiW/Estrategia.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using tp1;
+
+namespace tpfinal
+{
+    public class Estrategia
+    {
+        public String Consulta1(List<Proceso> datos)
+        {
+        	string result = "";
+        	
+        	Heap minHeap = new Heap(false);
+        	minHeap.BuildHeap(datos);
+
+        	Heap maxHeap = new Heap(true);
+        	maxHeap.BuildHeap(datos);
+            
+        	List<Proceso> hojasMinHeap = minHeap.GetHojas();
+        	List<Proceso> hojasMaxHeap = maxHeap.GetHojas();
+
+        	result += "Hojas de la MinHeap (SJF):\n\n";
+            
+        	foreach (Proceso hoja in hojasMinHeap)
+        	{
+        		result += "Nombre: " + hoja.nombre + ", Tiempo: " + hoja.tiempo + ", Prioridad: " + hoja.prioridad + "\n\n";
+        	}
+            
+        	result += "\n\n**************************************************************\n\n";
+        	result += "\nHojas de la MaxHeap (PPCSA):\n\n";
+            
+        	foreach (Proceso hoja in hojasMaxHeap)
+        	{
+        		result += "Nombre: " + hoja.nombre + ", Tiempo: " + hoja.tiempo + ", Prioridad: " + hoja.prioridad + "\n\n";
+        	}
+
+        	return result;
+        }
+        
+
+        public String Consulta2(List<Proceso> datos)
+        {
+        	string result = "";
+        	
+        	Heap minHeap = new Heap(false);
+        	minHeap.BuildHeap(datos);
+            
+        	Heap maxHeap = new Heap(true);
+        	maxHeap.BuildHeap(datos);
+
+        	int alturaMinHeap = minHeap.GetAltura();
+        	int alturaMaxHeap = maxHeap.GetAltura();
+            
+        	result += "Altura de las Heaps:\n\n";
+        	result += "Altura de la MinHeap: " + alturaMinHeap + "\n";
+        	result += "Altura de la MaxHeap: " + alturaMaxHeap + "\n";
+            
+        	return result;
+        }
+        
+        
+        public String Consulta3(List<Proceso> datos)
+        {
+        	string result = "";
+        	
+        	Heap minHeap = new Heap(false);
+        	minHeap.BuildHeap(datos);
+            
+        	Heap maxHeap = new Heap(true);
+        	maxHeap.BuildHeap(datos);
+
+        	result = "Procesos en Cada Nivel de la MinHeap (SJF):\n" + minHeap.RecorrerPorNiveles();
+        	result += "\n\n**************************************************************\n\n\n";
+        	result += "Procesos en Cada Nivel de la MaxHeap (PPCSA):\n" + maxHeap.RecorrerPorNiveles();
+            	
+        	return result;
+        }
+        
+        
+        public void ShortesJobFirst(List<Proceso> datos, List<Proceso> collected)
+        {
+        	Heap minHeap = new Heap(false);
+        	minHeap.BuildHeap(datos);
+        	
+        	while (!minHeap.EsVacia())
+        	{
+        		Proceso proceso = minHeap.Eliminar();
+        		collected.Add(proceso);
+        	}
+        }
+        
+        
+        public void PreemptivePriority(List<Proceso> datos, List<Proceso> collected)
+        {
+        	Heap maxHeap = new Heap(true);
+        	maxHeap.BuildHeap(datos);
+
+        	while (!maxHeap.EsVacia())
+        	{
+        		Proceso proceso = maxHeap.Eliminar();
+        		collected.Add(proceso);
+        	}
+        }
+    }
+}
---
+++ WiW/Heap.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using tp1;
+
+namespace tpfinal
+{
+	public class Heap
+	{
+		private List<Proceso> heap;
+		private bool esMaxHeap;
+		
+		
+		public Heap(bool esMaxHeap)
+		{
+			this.esMaxHeap = esMaxHeap;
+			heap = new List<Proceso>();
+		}
+		
+		
+		public void BuildHeap(List<Proceso> procesos)
+		{
+			heap.AddRange(procesos);
+            
+			for (int i = (heap.Count - 1) / 2; i >= 0; i--)
+			{
+				RestaurarOrden(i);
+			}
+		}
+		
+		
+		public void RestaurarOrden(int indice)
+		{
+			int indiceHijoIzquierdo = 2 * indice + 1;
+			int indiceHijoDerecho = 2 * indice + 2;
+			int indiceRaiz = indice;
+
+			if (indiceHijoIzquierdo < heap.Count && Comparar(indiceHijoIzquierdo, indiceRaiz))
+				indiceRaiz = indiceHijoIzquierdo;
+
+			if (indiceHijoDerecho < heap.Count && Comparar(indiceHijoDerecho, indiceRaiz))
+				indiceRaiz = indiceHijoDerecho;
+
+			if (indiceRaiz != indice) 
+			{
+				Proceso aux = heap[indice];
+				heap[indice] = heap[indiceRaiz];
+				heap[indiceRaiz] = aux;
+				RestaurarOrden(indiceRaiz);
+			}
+		}
+		
+		
+		public bool Comparar(int indiceHijo, int indicePadre)
+		{
+			if (esMaxHeap)
+				return heap[indiceHijo].prioridad > heap[indicePadre].prioridad;
+			else
+				return heap[indiceHijo].tiempo < heap[indicePadre].tiempo;
+		}
+		
+		
+		public Proceso Eliminar()
+		{
+			if (EsVacia())
+			{
+				throw new InvalidOperationException("La heap está vacía");
+			}
+
+			Proceso raiz = heap[0];
+			heap[0] = heap[heap.Count - 1];
+			heap.RemoveAt(heap.Count - 1);
+			RestaurarOrden(0);
+
+			return raiz;
+		}
+		
+		
+		public bool EsVacia()
+		{
+			return heap.Count == 0;
+		}
+		
+		
+		public int GetTamaño()
+		{
+			return heap.Count;
+		}
+		
+		
+		public List<Proceso> GetHojas()
+		{
+			List<Proceso> hojas = new List<Proceso>();
+			int tamaño = GetTamaño();
+			
+			for (int i = tamaño / 2; i < tamaño; i++)
+			{
+				hojas.Add(heap[i]);
+			}
+
+			return hojas;
+		}
+		
+		
+		public int GetAltura()
+		{
+			int altura = (int)Math.Floor(Math.Log(GetTamaño())/Math.Log(2));
+			
+			return altura;
+		}
+		
+		
+		public string RecorrerPorNiveles()
+		{
+			string texto = "";
+			
+			Cola<Proceso> cola = new Cola<Proceso>();
+			
+			cola.Encolar(heap[0]);
+			
+			int nivel = 0;
+			
+			while (!cola.EsVacia())
+			{
+				int tamañoNivel = cola.CantidadElementos();
+				
+				List<Proceso> procesosEnNivelActual = new List<Proceso>();
+				
+				for (int i = 0; i < tamañoNivel; i++)
+				{
+					Proceso proceso = cola.Desencolar();
+					procesosEnNivelActual.Add(proceso);
+					
+					int indice = heap.IndexOf(proceso);
+					int indiceHijoIzquierdo = 2 * indice + 1;
+					int indiceHijoDerecho = 2 * indice + 2;
+					
+					if (indiceHijoIzquierdo < heap.Count)
+						cola.Encolar(heap[indiceHijoIzquierdo]);
+					
+					if (indiceHijoDerecho < heap.Count)
+						cola.Encolar(heap[indiceHijoDerecho]);
+				}
+				
+				texto += "\n\nNivel " + nivel + ":\n";
+				
+				foreach(Proceso elem in procesosEnNivelActual)
+				{
+					texto += ("\nNombre: " + elem.nombre + ", Tiempo: " + elem.tiempo + ", Prioridad " + elem.prioridad + "\n");
+				}
+				
+				Console.WriteLine("\n*************************************************************************");
+				
+				nivel++;
+			}
+			
+			return texto;
+		}
+	}
+}
